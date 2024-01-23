430c91eb5f50a24d10b8a94a66011aac24d2a045
Nikulin <s.b.nikuliny@gmail.com>
2024-01-23 10:45:11 +0700

    welcome_message

--- app.py
+++ app.py
@@ -14,6 +14,22 @@ async def start_cmd(message):
     await message.answer('Start')
 
 
+# @dp.message()
+# async def echo(message):
+#     await message.answer(message.text)
+
+
+@dp.message()
+async def welcome_message(message):
+    text = message.text.lower()
+    if text in ['привет', 'здрвавствуйте', 'hello', 'hi']:
+        await message.answer('Hello!')
+    elif text in ['пока', 'досвидания', 'while']:
+        await message.answer('Пока!')
+    else:
+        await message.answer(message.text)
+
+
 async def main():
     await dp.start_polling(bot)
 
